dd2872c712c3333b358fc7f6817f816f76c7c6cb
Al Avery <averyab0@sewanee.edu>
2017-09-03 15:32:59 -0500

    Delete commented out line

--- main.py
+++ main.py
@@ -8,6 +8,5 @@ target_price, dishes = input_output.get_csv_data(csv_file_path)
 if target_price == 0:
     sys.exit('Target price is zero -- solution is an order of zero dishes.')
 
-dish_combination = find_combination.find_dish_combination1(target_price, dishes)
-# dish_combination = find_combination.find_dish_combination2(target_price, dishes) # different algorithm
+dish_combination = find_combination.find_dish_combination(target_price, dishes)
 input_output.print_dish_combination(dish_combination)
